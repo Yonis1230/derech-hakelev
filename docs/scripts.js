680b18302593e7632c054b126a30471f33f2da3a
Yonis1230 <161774106+Yonis1230@users.noreply.github.com>
2024-04-16 11:30:27 +0300

    Add files via upload

---
+++ docs/scripts.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var slides = document.querySelectorAll(".slide");
+  var currentIndex = 0;
+  var prevButton = document.querySelector(".prev");
+  var nextButton = document.querySelector(".next");
+  var slideInterval = setInterval(function () {
+    changeSlide(1);
+  }, 5000);
+
+  function showSlide(index) {
+    slides.forEach(function (slide, idx) {
+      slide.style.display = idx === index ? "block" : "none";
+    });
+  }
+
+  function changeSlide(step) {
+    currentIndex = (currentIndex + step + slides.length) % slides.length;
+    showSlide(currentIndex);
+    resetInterval();
+  }
+
+  function resetInterval() {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(function () {
+      changeSlide(1);
+    }, 5000);
+  }
+
+  prevButton.addEventListener("click", function () {
+    changeSlide(-1);
+  });
+  nextButton.addEventListener("click", function () {
+    changeSlide(1);
+  });
+});
