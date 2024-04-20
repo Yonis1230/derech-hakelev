5bf485a73d99c4b89fc58b55fe7baae10daa622a
Yonis1230 <161774106+Yonis1230@users.noreply.github.com>
2024-04-20 21:39:23 +0300

    Update scripts.js

--- docs/scripts.js
+++ docs/scripts.js
@@ -101,7 +101,7 @@ document.addEventListener('DOMContentLoaded', function() {
               card.style.backgroundImage = 'none'; // הסר את התמונה
             });
           }
-        }, 2000);
+        }, 3000);
       }
     }
 });
